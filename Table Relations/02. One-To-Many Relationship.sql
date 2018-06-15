2d88fa8237ec5ee35cebda69a29565f514bb4b0f
Adem <ademcran4aliev@abv.bg>
2018-06-15 17:49:33 +0300

    Fixed

--- Table Relations/02. One-To-Many Relationship.sql
+++ Table Relations/02. One-To-Many Relationship.sql
@@ -1,24 +1,20 @@
-
 CREATE TABLE Manufacturers
 (
-  ManufacturerID INT PRIMARY KEY NOT NULL,
-  Name NVARCHAR(255) NOT NULL,
+  ManufacturerID INT PRIMARY KEY ,
+  Name NVARCHAR(255) ,
   EstablishedOn DATE 
 
 )
 
 CREATE TABLE Models
 (
-  ModelID INT PRIMARY KEY NOT NULL ,
-  Name NVARCHAR(255) NOT NULL,
-  ManufacturersID INT REFERENCES Manufacturers(ManufacturerID) NOT NULL
+  ModelID INT PRIMARY KEY ,
+  Name NVARCHAR(255) ,
+  ManufacturerID INT REFERENCES Manufacturers(ManufacturerID) 
 )
 
 INSERT INTO Manufacturers(ManufacturerID,Name,EstablishedOn)
 VALUES (1,'BMW','07/03/1916'),(2,'Tesla','01/01/2003'),(3,'Lada','01/05/1966')
 
-INSERT INTO Models(ModelID,Name,ManufacturersID)
+INSERT INTO Models(ModelID,Name,ManufacturerID)
 VALUES (101,'X1',1),(102,'i6',1),(103,'Model S',2),(104,'Model X',2),(105,'Model 3',2),(106,'Nova',3)
-
-
-
